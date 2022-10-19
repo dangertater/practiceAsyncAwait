c38d6ae4e9553de0a4e57a5e180e9ddc331f5987
dangertater <106702944+dangertater@users.noreply.github.com>
2022-10-19 13:50:27 -0600

    just starting

---
+++ script.js
@@ -0,0 +1,13 @@
+//https://www.youtube.com/watch?v=PoRJizFvM7s
+let object0 = { name: "first", age: 0, skills: "first of his name" }
+let object1 = { name: "second", age: 1, skills: "two feet for walking" }
+let object2 = { name: "third", age: 2, skills: "third hand available for use" }
+//idk why object3 gets prettiered?
+let object3 = {
+	name: "fourth",
+	age: 3,
+	skills: "four horseman are ready to plague",
+}
+let objectArray = [object0, object1, object2, object3]
+
+//will take the array of objects and makes an objectArrayEven and ObjectArrayOdd based on objects name
