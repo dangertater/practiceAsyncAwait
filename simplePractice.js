a8559e59b7cc99ac42865b4eee75c87a1abcf8b5
dangertater <106702944+dangertater@users.noreply.github.com>
2022-10-21 10:13:12 -0600

    pause button functioning

--- simplePractice.js
+++ simplePractice.js
@@ -1,53 +1,73 @@
 let body = document.getElementsByTagName("body")
 let stylesFromCSS = getComputedStyle(body[0])
 let randomNum = Math.floor(Math.random() * 3)
+let buttonStop = document.getElementById("buttonStop")
+let backgroundChanging = true
 
+//pause/play button for the background color
+buttonStop.addEventListener("click", (e) => {
+	if (backgroundChanging === true) {
+		backgroundChanging = false
+	} else {
+		backgroundChanging = true
+		randomColorSelector()
+	}
+})
 let changeBackgroundToRed = () => {
 	setTimeout(() => {
 		body[0].style.backgroundColor = "red"
-		randomColorSelector()
-	}, 2000)
+		if (backgroundChanging === false) {
+		} else {
+			randomColorSelector()
+		}
+	}, 750)
 }
 
 let changeBackgroundToBlue = () => {
 	setTimeout(() => {
 		body[0].style.backgroundColor = "blue"
-		randomColorSelector()
-	}, 2000)
+		if (backgroundChanging === false) {
+		} else {
+			randomColorSelector()
+		}
+	}, 750)
 }
 
 let changeBackgroundToGreen = () => {
 	setTimeout(() => {
 		body[0].style.backgroundColor = "green"
-		randomColorSelector()
-	}, 2000)
+		if (backgroundChanging === false) {
+		} else {
+			randomColorSelector()
+		}
+	}, 750)
 }
 
-//goal of promise is for the promise to resolve if font is yellow, and reject if not
-//----randomColorSelector is used for the background color, but is called frequently so
-//----promise will also be called frequently. first loop through it resolves
-//----.then (line 52) call fontColorHotpink(), next cycle through randomColorSelector it
-//----is hotpink, and the computer is just like naaaaaah it's yellow now
 let randomColorSelector = () => {
-	return new Promise((resolve, reject) => {
-		if ((body[0].style.color = "yellow")) {
-			resolve()
-		} else {
-			reject("your font color is not yellow dawg")
-		}
-		randomNum = Math.floor(Math.random() * 3)
-		if (randomNum === 0) {
-			changeBackgroundToBlue()
-		} else if (randomNum === 1) {
-			changeBackgroundToRed()
-		} else {
-			changeBackgroundToGreen()
-		}
-	})
+	randomNum = Math.floor(Math.random() * 3)
+	if (randomNum === 0) {
+		changeBackgroundToBlue()
+	} else if (randomNum === 1) {
+		changeBackgroundToRed()
+	} else {
+		changeBackgroundToGreen()
+	}
+}
+
+let fontColorYellow = (e) => {
+	body[0].style.color = "yellow"
+	console.log(e)
 }
 
 let fontColorHotpink = () => {
 	body[0].style.color = "hotpink"
 }
 
-randomColorSelector().then(fontColorHotpink())
+randomColorSelector()
+
+//below is the goal for promise reject after I failed miserably and deleted all my code
+//----goal of promise is for the promise to resolve if font is yellow, and reject if not
+//--------randomColorSelector is used for the background color, but is called frequently so
+//--------promise will also be called frequently. first loop through it resolves
+//--------.then (line 52) call fontColorHotpink(), next cycle through randomColorSelector it
+//--------is hotpink, and the computer is just like naaaaaah it's yellow now
